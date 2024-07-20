a0fa44f9d16a38e277de8bae19d7213af13dc32f
António António <antonioandroid3a@gmail.com>
2024-07-20 21:37:27 +0100

    create docker portainer-containers.sh file

---
+++ docker/portainer-containers.sh
@@ -0,0 +1,24 @@
+ #Portainer
+docker run -d \
+ --restart unless-stopped \
+ --name antonio3a-portainer \
+ --net antonio3a-net \
+ --hostname antonio3a-portainer \
+ -p 8000:8000 \
+ -p 9000:9000 \
+ -p 9443:9443 \
+ -v /var/run/docker.sock:/var/run/docker.sock \
+ -v antonio3a-portainer-data-vol:/data \
+ portainer/portainer-ce:2.20.3-alpine
+
+
+ # Portainer agent
+  docker run -d \
+  -p 9001:9001 \
+  --net aaa-net \
+  --name aaa-portainer-agent \
+  --hostname aaa-portainer-agent \
+  --restart always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+  portainer/agent:2.20.3-alpine
