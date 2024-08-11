14cc3f12fabe9f379594d19324da267267e676ad
António António <antonioandroid3a@gmail.com>
2024-08-11 04:07:24 +0100

    update CN in ssl script

--- openssl/script.sh
+++ openssl/script.sh
@@ -1,5 +1,5 @@
 ###Gen auto signed Crt & Key
 openssl req -x509 -newkey rsa:4096 -sha256 -days 36500 \
   -nodes -keyout antonio3a.aaa.key -out antonio3a.aaa.crt \
-  -subj "/C=AO/ST=Luanda/L=Luanda/O=3A Group Inc./OU=3A Dev LLC/CN=antonio3a.aaa" \
+  -subj "/C=AO/ST=Luanda/L=Luanda/O=3A Group Inc./OU=3A Dev LLC/CN=António António - 3A" \
   -addext "subjectAltName=DNS:antonio3a.aaa,DNS:*.antonio3a.aaa"
