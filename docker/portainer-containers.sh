55cec421fc06dae03bb01d09cd554ece640fd363
António António <antonioandroid3a@gmail.com>
2024-11-21 18:10:21 +0100

    update portainer container properties

--- docker/portainer-containers.sh
+++ docker/portainer-containers.sh
@@ -1,16 +1,13 @@
  #Portainer
 docker run -d \
- --restart unless-stopped \
+ --restart always \
  --name antonio3a-portainer \
- --net antonio3a-network \
- --hostname antonio3a-portainer \
- -p 8000:8000 \
+ --net antonio3a-bridge \
+ --hostname portainer.antonio3a.aaa \
  -p 9000:9000 \
- -p 9443:9443 \
  -v /var/run/docker.sock:/var/run/docker.sock \
  -v antonio3a-portainer-data-vol:/data \
- portainer/portainer-ce:2.21.4
-
+ portainer/portainer-ce:2.22.0
 
  # Portainer agent
   docker run -d \
@@ -21,4 +18,4 @@ docker run -d \
   --restart always \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /var/lib/docker/volumes:/var/lib/docker/volumes \
-  portainer/agent:2.20.3-alpine
+  portainer/agent:2.22.0-alpine
